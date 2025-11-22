644b73d6b0c607900aa6e09b0ca7c30fdd8a168a
bb1950328 <bb1950328@gmail.com>
2025-11-22 23:39:54 +0100

    update Node.js from 18 to 24 in CI build reformat code

--- src/drag_drop.ts
+++ src/drag_drop.ts
@@ -15,51 +15,51 @@ export enum ObjectType {
 }
 
 type DragData = {
-    type: ObjectType;
-    id: number | string;
-    cursorOffsetX: number;
-    cursorOffsetY: number;
+  type: ObjectType;
+  id: number | string;
+  cursorOffsetX: number;
+  cursorOffsetY: number;
 };
 
 export function startDrag(
-    event: DragEvent,
-    type: ObjectType,
-    id: number | string,
-    cursorOffsetX: number = 0,
-    cursorOffsetY: number = 0,
+  event: DragEvent,
+  type: ObjectType,
+  id: number | string,
+  cursorOffsetX: number = 0,
+  cursorOffsetY: number = 0,
 ) {
-    const data: DragData = {
-        type: type,
-        id: id,
-        cursorOffsetX: cursorOffsetX,
-        cursorOffsetY: cursorOffsetY,
-    };
-    event.dataTransfer!.setData(encodeUpperCase(JSON.stringify(data)), "");
+  const data: DragData = {
+    type: type,
+    id: id,
+    cursorOffsetX: cursorOffsetX,
+    cursorOffsetY: cursorOffsetY,
+  };
+  event.dataTransfer!.setData(encodeUpperCase(JSON.stringify(data)), "");
 }
 
 export function allowDrop(event: DragEvent, ...types: ObjectType[]) {
-    const data: DragData = getDragData(event);
-    if (types.includes(data.type)) {
-        event.preventDefault();
-    }
+  const data: DragData = getDragData(event);
+  if (types.includes(data.type)) {
+    event.preventDefault();
+  }
 }
 
 export function getDragData(event: DragEvent): DragData {
-    const text = decodeUpperCase(event.dataTransfer!.types[0] ?? "");
-    console.debug(text);
-    if (text[0] == "{") {
-        return JSON.parse(text);
-    } else {
-        return {} as DragData;
-    }
+  const text = decodeUpperCase(event.dataTransfer!.types[0] ?? "");
+  console.debug(text);
+  if (text[0] == "{") {
+    return JSON.parse(text);
+  } else {
+    return {} as DragData;
+  }
 }
 
 export function getDraggedIdInt(event: DragEvent): number {
-    const data = getDragData(event);
-    if (typeof data.id == "string") {
-        return parseInt(data.id);
-    }
-    return data.id;
+  const data = getDragData(event);
+  if (typeof data.id == "string") {
+    return parseInt(data.id);
+  }
+  return data.id;
 }
 
 export function getTwoDraggedIds(event: DragEvent): [number, number] {
@@ -70,38 +70,38 @@ export function getTwoDraggedIds(event: DragEvent): [number, number] {
 }
 
 export function allowDropDeletableElements(event: DragEvent) {
-    allowDrop(event, ObjectType.TOUR_ELEMENT, ObjectType.ASSIGNED_JWLER);
+  allowDrop(event, ObjectType.TOUR_ELEMENT, ObjectType.ASSIGNED_JWLER);
 }
 
 export function deleteDroppedElement(event: DragEvent) {
-    const dragData = getDragData(event);
-    const store = useStore();
-    switch (dragData.type) {
-        case ObjectType.TOUR_ELEMENT: {
-            const [tourId, elementId] = getTwoDraggedIds(event);
-            popTourElement(tourId, elementId);
-            break;
-        }
-        case ObjectType.ASSIGNED_JWLER: {
-            const [tourId, jwlerId] = getTwoDraggedIds(event);
-            const tour = store.data.tours.get(tourId)!;
-            tour.jwlers = tour.jwlers!.filter(id => id != jwlerId);
-            break;
-        }
+  const dragData = getDragData(event);
+  const store = useStore();
+  switch (dragData.type) {
+    case ObjectType.TOUR_ELEMENT: {
+      const [tourId, elementId] = getTwoDraggedIds(event);
+      popTourElement(tourId, elementId);
+      break;
     }
+    case ObjectType.ASSIGNED_JWLER: {
+      const [tourId, jwlerId] = getTwoDraggedIds(event);
+      const tour = store.data.tours.get(tourId)!;
+      tour.jwlers = tour.jwlers!.filter(id => id != jwlerId);
+      break;
+    }
+  }
 }
 
 export function dropTourElement(event: DragEvent, newTour: Tour, newStart: Date) {
-    event.stopPropagation();
-    const [oldTourId, elementId] = getTwoDraggedIds(event);
-    const oldElement = popTourElement(oldTourId, elementId);
-    const newElement = {
-      id: oldElement.id,
-      tour: newTour.id!,
-      start: newStart,
-      end: new Date(newStart.getTime() + oldElement.end.getTime() - oldElement.start.getTime()),
-      type: oldElement.type,
-      client: oldElement.client,
-    } as TourElement;
-    addTourElement(newTour.id!, newElement);
+  event.stopPropagation();
+  const [oldTourId, elementId] = getTwoDraggedIds(event);
+  const oldElement = popTourElement(oldTourId, elementId);
+  const newElement = {
+    id: oldElement.id,
+    tour: newTour.id!,
+    start: newStart,
+    end: new Date(newStart.getTime() + oldElement.end.getTime() - oldElement.start.getTime()),
+    type: oldElement.type,
+    client: oldElement.client,
+  } as TourElement;
+  addTourElement(newTour.id!, newElement);
 }
